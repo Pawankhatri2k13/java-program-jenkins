7eb404202b19efc99d9521b6cc0fc6030251df57
Pawan Dev <pawankhatri2k13@gmail.com>
2023-12-16 23:41:11 +0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ class Test{
 public static void main(String arg[]){
   System.out.println("Hello Pawan");
   System.out.println("Hello Jetmal");
+  System.out.println("Hello Jetmal");
+  System.out.println("Hello Jetmal Singh");
 
  }
 }
